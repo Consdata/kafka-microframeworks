de9d75e669ce7a8827bd3d80cd05437eaf878ff6
mmergo <mmergo@consdata.com>
2022-02-27 20:10:22 +0100

    stub spring boot application

---
+++ kafka-microframeworks-spring-boot/src/main/java/com/consdata/kafka/microframeworks/springboot/KafkaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.consdata.kafka.microframeworks.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringBootApplication.class, args);
+	}
+
+}
