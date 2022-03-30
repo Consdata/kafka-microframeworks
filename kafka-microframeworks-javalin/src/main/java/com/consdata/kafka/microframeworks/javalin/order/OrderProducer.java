93ed3623fabebc2ea5e9429418953f3676dce1cd
mmergo <mmergo@consdata.com>
2022-03-30 19:26:53 +0200

    javalin producer

--- kafka-microframeworks-javalin/src/main/java/com/consdata/kafka/microframeworks/javalin/order/OrderProducer.java
+++ kafka-microframeworks-javalin/src/main/java/com/consdata/kafka/microframeworks/javalin/order/OrderProducer.java
@@ -5,7 +5,6 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.connect.json.JsonSerializer;
 
 import java.util.Properties;
 
